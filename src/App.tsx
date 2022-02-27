6be196e295c0150bb5ce94deab259a44d6341107
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2022-02-27 20:25:19 +1100

    feat: add tailwind css
    as per https://tailwindcss.com/docs/guides/vite

--- src/App.tsx
+++ src/App.tsx
@@ -13,17 +13,15 @@ const App: Component = () => {
   };
 
   return (
-    <div>
-      Email user: <textarea id="username"></textarea>
-      <br />
-      <br />
+    <div class="grid grid-flow-row-dense grid-cols-3 grid-rows-2">
+      <span>Email</span><textarea id="username" class="border-2 rounded" placeholder="name"></textarea>
       <button
+        class="px-5 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent"
         onClick={() => ws.send(document.getElementById("username").value)}
       >
         Send
       </button>
-      <br />
-      Server message: {message()}
+      <div class="col-span-3 text-center">{message()}</div>
     </div>
   );
 };
